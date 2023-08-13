1bccddaada98f772e2ca71ea80712f417842edcd
kiransai <kiransais03@gmail.com>
2023-08-13 23:58:30 +0530

    submitting code-Sun Aug 13 2023 23:58:27 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,47 @@
 <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
+import React, { useState } from "react";
+import Reviews from "./Reviews";
+import "../styles/App.css"
+
+const App = ()=>{
+
+    let [currid,setCurrid] = useState(1);
+    
+    function btnclicked(e,ip) {
+        if(ip===-1)
+        {
+            if(currid===1)
+            {
+               setCurrid(4);
+               return;
+            }
+            setCurrid(currid-1);
+        }
+        else {
+            if(currid===4)
+            {
+               setCurrid(1);
+               return;
+            }
+            setCurrid(currid+1);
+        }
+    }
+
+    function randombutton (e) {
+       let val=parseInt(Math.random()*4+1);
+       setCurrid(val)
+    }
+
+    return(<div>
+    <h1 id="review-heading">Our Reviews</h1>
+    <button className="random-btn" onClick={randombutton}>Surprise Me</button>
+    <div className="total-box">
+        <button onClick={(e)=>{btnclicked(e,-1)}} className="prev-btn">Previous</button>
+        <Reviews currid={currid} setId={setCurrid}/>
+        <button onClick={(e)=>{btnclicked(e,1)}} className="next-btn">Next</button>
+    </div>
+    </div>)
+}
+
+export default App;
